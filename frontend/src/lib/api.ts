18c75f3fae79a63099bd978db4124bd7344d1096
Anurudh-Agarwal <anurudhagarwal102@gmail.com>
2026-09-04 00:20:01 +0530

    chore: use backend URL from environment variable

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -20,8 +20,8 @@ import {
   SyncCodeChefResponse,
 } from "@/types/api";
 
-//const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+//const API_BASE_URL = "http://localhost:5000";
 
 const apiCall = async <T>(
   endpoint: string,
